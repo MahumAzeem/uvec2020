5bcf337dbd861a4812b9f12d2fe202ff1911d221
Mahum Azeem <mahum@revela.io>
2020-07-04 12:01:29 -0700

    Basics of fast api set up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # uvec2020
+How to build & run locally:
+pip install -r requirements.txt
+cd api
+uvicorn server:app --reload
--- api/server.py
+++ api/server.py
@@ -5,12 +5,26 @@ See README for running instructions.
 """
 import importlib
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path, Query
 
 import api_spec
 #from storage import database
 
 app = FastAPI()
 
-# @app.post("/board", response_model=??, response_model_exclude_unset=True)
-# async def generate(request: ??):
+@app.get("/all_tasks")
+async def get_all_tasks():
+    results = {1,2,3}
+        #get tasks from db, filler data rn
+
+    return results
+
+
+@app.get("/")
+async def root():
+    """
+    Root route. Currently just responds to a GET request with a message.
+    Useful for testing that your server is working; will require a
+    different response before deployment.
+    """
+    return {"message": "yes this is dog"}
