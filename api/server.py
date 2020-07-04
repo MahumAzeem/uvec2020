b42266251cb1d09c0b6b8f3066b489b7cc3369f4
Mahum Azeem <mahum@revela.io>
2020-07-04 13:08:46 -0700

    Added models

--- api/server.py
+++ api/server.py
@@ -7,7 +7,8 @@ import importlib
 
 from fastapi import FastAPI, HTTPException, Path, Query
 
-import api_spec
+from task import Task
+from employee import Employee
 #from storage import database
 
 app = FastAPI()
@@ -19,6 +20,25 @@ async def get_all_tasks():
 
     return results
 
+@app.get("/task/{id}")
+async def get_task(id: int) -> Task:
+    """
+    GET route for getting the a specific task.
+
+    Args:
+        id: The id of the task required.
+
+    Returns:
+        A JSON response contaning a task object.
+    """
+    #example task:
+    task = Task(
+        task_id= 1,
+        title="Example Task",
+        creator= 21
+    )
+    return task
+
 
 @app.get("/")
 async def root():
@@ -27,4 +47,4 @@ async def root():
     Useful for testing that your server is working; will require a
     different response before deployment.
     """
-    return {"message": "yes this is dog"}
+    return {"message": "Welcome to Kira"}
---
+++ api/task.py
@@ -0,0 +1,17 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+class Task(BaseModel):
+   task_id: int
+   title: str
+   description: str = None
+   priority: str = None
+   status: str = None
+   creator: int #employee id
+   assigned_to: List[int] = None
+   completion_time: int = None
+   blocks_tasks: List[int]=None
+   blocked_by: List[int] = None
+
+
+
